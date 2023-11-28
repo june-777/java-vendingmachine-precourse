34be1204fca69a560ec59bd493b97408347e52d7
june-777 <wlwhswnsrl96@gmail.com>
2023-11-29 02:24:41 +0900

    refactor(ExceptionHandler): 예외 메시지를 출력하도록 변경

--- src/main/java/vendingmachine/controller/ExceptionHandler.java
+++ src/main/java/vendingmachine/controller/ExceptionHandler.java
@@ -4,11 +4,11 @@ import java.util.function.Supplier;
 
 public class ExceptionHandler {
     public static <T> T handle(Supplier<T> supplier) {
-        while(true) {
+        while (true) {
             try {
                 return supplier.get();
             } catch (IllegalArgumentException e) {
-                System.out.println(e);
+                System.out.println(e.getMessage());
             }
         }
     }
