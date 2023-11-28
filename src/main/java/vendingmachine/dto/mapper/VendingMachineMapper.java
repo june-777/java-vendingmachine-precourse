46753f442108eb089c93fa0b4b685f407ea71933
june-777 <wlwhswnsrl96@gmail.com>
2023-11-29 02:27:23 +0900

    style(VendingMachineMapper): 정적 팩터리 메서드로 인한 코드 수정

--- src/main/java/vendingmachine/dto/mapper/VendingMachineMapper.java
+++ src/main/java/vendingmachine/dto/mapper/VendingMachineMapper.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import vendingmachine.domain.Coin;
+import vendingmachine.domain.Coins;
 import vendingmachine.domain.Product;
 import vendingmachine.domain.VendingMachine;
 import vendingmachine.dto.request.ProductLineRequest;
@@ -19,12 +20,12 @@ public class VendingMachineMapper {
             Product product = toProduct(productLineRequest);
             products.add(product);
         }
-
-        return new VendingMachine(coinCounts, products);
+        Coins coins = new Coins(coinCounts);
+        return new VendingMachine(coins, products);
     }
 
     private Product toProduct(final ProductLineRequest productLineRequest) {
-        return new Product(
+        return Product.from(
                 productLineRequest.getName(),
                 productLineRequest.getPrice(),
                 productLineRequest.getCount());
