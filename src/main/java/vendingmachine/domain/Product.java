a1b0343e30d6534c50592c99f2b25284d1cdd085
june-777 <wlwhswnsrl96@gmail.com>
2023-11-28 16:46:56 +0900

    feat(Product): 자판기 상품을 의미하는 클래스

---
+++ src/main/java/vendingmachine/domain/Product.java
@@ -0,0 +1,41 @@
+package vendingmachine.domain;
+
+import java.util.Objects;
+
+public class Product {
+    private final String name;
+    private final int price;
+    private final int count;
+
+    public Product(String name, int price, int count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", count=" + count +
+                '}';
+    }
+}
