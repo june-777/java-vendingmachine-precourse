42cce52d43856d42984e0331bef3a1fa6dee4bde
june-777 <wlwhswnsrl96@gmail.com>
2023-11-28 16:48:17 +0900

    feat(VendingMachineException): 중복 상품 예외 Enum 추가

--- src/main/java/vendingmachine/exception/VendingMachineException.java
+++ src/main/java/vendingmachine/exception/VendingMachineException.java
@@ -1,7 +1,8 @@
 package vendingmachine.exception;
 
 public enum VendingMachineException {
-    INVALID_AMOUNT("보유 금액으로 동전을 생성할 수 없습니다.");
+    INVALID_AMOUNT("보유 금액으로 동전을 생성할 수 없습니다."),
+    DUPLICATE_PRODUCT("중복된 상품이 존재합니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
