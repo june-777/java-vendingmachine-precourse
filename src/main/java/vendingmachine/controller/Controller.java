78470ec3a9c5e21d9530275acd49127eb4115aa3
june-777 <wlwhswnsrl96@gmail.com>
2023-11-29 02:28:00 +0900

    feat(Controller): 로직 순서에 맞게 구현

--- src/main/java/vendingmachine/controller/Controller.java
+++ src/main/java/vendingmachine/controller/Controller.java
@@ -2,6 +2,10 @@ package vendingmachine.controller;
 
 import java.util.EnumMap;
 import vendingmachine.domain.Coin;
+import vendingmachine.domain.Product;
+import vendingmachine.domain.VendingMachine;
+import vendingmachine.domain.VendingMachineStatus;
+import vendingmachine.dto.request.ProductRequest;
 import vendingmachine.service.VendingService;
 import vendingmachine.view.input.InputView;
 import vendingmachine.view.output.OutputView;
@@ -19,11 +23,14 @@ public class Controller {
     }
 
     public void process() {
-        EnumMap<Coin, Integer> coinCounts = setVenderCoin();
+        EnumMap<Coin, Integer> coinCounts = setVendingCoin();
         renderingCoin(coinCounts);
+
+        VendingMachine vendingMachine = setVendingProducts(coinCounts);
+        executeVendingMachine(vendingMachine);
     }
 
-    private EnumMap<Coin, Integer> setVenderCoin() {
+    private EnumMap<Coin, Integer> setVendingCoin() {
         return ExceptionHandler.handle(() -> {
             int venderAmount = inputView.readVenderAmount();
             return vendingService.changeToCoin(venderAmount);
@@ -32,5 +39,41 @@ public class Controller {
 
     private void renderingCoin(EnumMap<Coin, Integer> coinCounts) {
         outputView.printVenderCoin(coinCounts);
+    }
+
+    private VendingMachine setVendingProducts(EnumMap<Coin, Integer> coinCounts) {
+        return ExceptionHandler.handle(() -> {
+            String productsInfo = inputView.readProducts();
+            ProductRequest productRequest = ProductRequest.of(productsInfo);
+            return vendingService.createVendingMachine(coinCounts, productRequest);
+        });
+    }
+
+    private void executeVendingMachine(VendingMachine vendingMachine) {
+        int amount = setPutAmount();
+
+        while (vendingMachine.calculateStatus(amount) != VendingMachineStatus.CLOSE) {
+            Product buyProduct = setBuyProduct(amount, vendingMachine);
+            amount = vendingMachine.sell(buyProduct, amount);
+        }
+        EnumMap<Coin, Integer> remainCoinCounts = vendingMachine.changeCoins(amount);
+        renderingChanges(amount, remainCoinCounts);
+    }
+
+    private int setPutAmount() {
+        return ExceptionHandler.handle(inputView::readPutAmount);
+    }
+
+    private Product setBuyProduct(final int amount, final VendingMachine vendingMachine) {
+        return ExceptionHandler.handle(() -> {
+            outputView.printAmount(amount);
+            String buyProduct = inputView.readBuyProduct(amount);
+            return vendingMachine.getProduct(buyProduct);
+        });
+    }
+
+    private void renderingChanges(int amount, EnumMap<Coin, Integer> remainCoinCounts) {
+        outputView.printAmount(amount);
+        outputView.printChange(remainCoinCounts);
     }
 }
