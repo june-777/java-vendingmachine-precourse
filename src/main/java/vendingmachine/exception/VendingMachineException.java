64ef708454ae731876ec5f4dc6bf47f9eb1e8c6f
june-777 <wlwhswnsrl96@gmail.com>
2023-11-29 02:21:38 +0900

    feat(VendingMachineException): 자판기 관련 예외 Enum 추가

--- src/main/java/vendingmachine/exception/VendingMachineException.java
+++ src/main/java/vendingmachine/exception/VendingMachineException.java
@@ -5,7 +5,11 @@ public enum VendingMachineException {
     DUPLICATE_PRODUCT("중복된 상품이 존재합니다."),
     INVALID_PRODUCT_COUNT("상품 등록 시, 상품의 개수는 1개 이상이어야 합니다."),
     INVALID_PRODUCT_MIN_PRICE("상품 등록 시, 상품 가격은 100원부터 시작해야 합니다."),
-    INVALID_PRODUCT_PRICE_UNIT("상품 등록 시, 상품 가격은 10원으로 나누어 떨어져야 합니다.");
+    INVALID_PRODUCT_PRICE_UNIT("상품 등록 시, 상품 가격은 10원으로 나누어 떨어져야 합니다."),
+
+    NON_EXIST_PRODUCT("주문한 상품은 존재하지 않는 상품입니다."),
+    OUT_OF_STOCK_PRODUCT("주문한 상품의 수량이 다 떨어졌습니다."),
+    EXPENSIVE_PRODUCT("남은 금액으로 살 수 없는 상품입니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
