f2934a1c0daaa291ed59d0cd45327835eb0869ea
june-777 <wlwhswnsrl96@gmail.com>
2023-11-28 16:50:53 +0900

    feat(InputView): 자판기 상품 정보를 입력하는 기능

--- src/main/java/vendingmachine/view/input/InputView.java
+++ src/main/java/vendingmachine/view/input/InputView.java
@@ -10,12 +10,23 @@ public class InputView {
         System.out.println(Message.VENDER_AMOUNT.message);
         String input = Console.readLine().trim();
         NumberValidator.validate(input);
+
         return Integer.parseInt(input);
+    }
+
+    public String readProducts() {
+        System.out.println();
+        System.out.println(Message.VENDING_PRODUCT.message);
+        String productsInput = Console.readLine().trim();
+        FormValidator.validate(productsInput);
+
+        return productsInput;
     }
 
     private enum Message {
         //TODO
-        VENDER_AMOUNT("자판기가 보유하고 있는 금액을 입력해 주세요.");
+        VENDER_AMOUNT("자판기가 보유하고 있는 금액을 입력해 주세요."),
+        VENDING_PRODUCT("상품명과 가격, 수량을 입력해 주세요.");
 
         private final String message;
 
