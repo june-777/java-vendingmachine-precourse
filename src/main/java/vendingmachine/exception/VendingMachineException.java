bbe46cc42e141c933076b36e69371d3ce3867695
june-777 <wlwhswnsrl96@gmail.com>
2023-11-28 15:33:20 +0900

    feat(VendingMachineException): 자판기 관련 예외 Enum

---
+++ src/main/java/vendingmachine/exception/VendingMachineException.java
@@ -0,0 +1,20 @@
+package vendingmachine.exception;
+
+public enum VendingMachineException {
+    INVALID_AMOUNT("보유 금액으로 동전을 생성할 수 없습니다.");
+
+    private static final String PREFIX = "[ERROR] ";
+    private final String message;
+
+    VendingMachineException(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return PREFIX + message;
+    }
+
+    public IllegalArgumentException make() {
+        return new IllegalArgumentException(getMessage());
+    }
+}
